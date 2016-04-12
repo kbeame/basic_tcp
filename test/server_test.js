3580a244e6fef49684104f4f7ad6f2be6e3fa038
Katherine Beame <katherine.beame@gmail.com>
2016-04-11 22:08:51 -0700

    small linting

--- test/server_test.js
+++ test/server_test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const server = require (__dirname + '/../server.js');
+const server = require(__dirname + '/../server.js');
 
 describe('should connect to the server', () => {
   it('should go to port 3000', (done) => {
