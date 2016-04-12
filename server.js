c56a4241c00a60a7cec1155d925a8440c46c8d14
Katherine Beame <katherine.beame@gmail.com>
2016-04-12 10:56:32 -0700

    changed the test to read the number of file in a folder

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const server = net.createServer((socket) => {
   socket.on('data', (chunk) => {
     var unique = new Date();
     unique = unique.toISOString();
-    console.log('Unique ' + unique);
-    console.log('socket ' + socket);
+    // console.log('Unique ' + unique);
+    // console.log('socket ' + socket);
     fs.writeFile(__dirname + '/newfiles/' + unique + '.txt', chunk, (err) => {
       if (err) throw err;
       console.log('New file created');
-      socket.end((err, res) => {
-        assert(200 == res.status);
-      });
+      socket.end();
   });
+});
 }).listen(3000, () => {
   process.stdout.write('<3 Server up on 3000 :-)');
-})
+});
 
 module.exports = server;
--- test/server_test.js
+++ test/server_test.js
@@ -1,23 +1,36 @@
 const chai = require('chai');
+const fs = require('fs');
 const expect = chai.expect;
 const server = require(__dirname + '/../server.js');
 const chaiHTTP = require('chai-http');
 chai.use(chaiHTTP);
 
 describe('should connect to the server', () => {
+  var files;
+  var newFiles;
   before(() => {
     server.listen(3000);
+    files = fs.readdirSync(__dirname + '/../newfiles/', (err, files) => {
+      if (err) throw err;
+    });
+    console.log('files.length ' + files.length);
+    files = files.length + 1;
+    console.log(files);
   });
   after(() => {
     server.close();
   });
 
-  it('should go to port 3000', (done) => {
+  it('the number of file should increase', (done) => {
     chai.request('http://localhost:3000')
     .get('/')
-    .end((err, response) => {
-      console.log(res);
-      expect((res).to.have.status(200));
+    .end(() => {
+      newFiles = fs.readdirSync(__dirname + '/../newfiles/', (err, files) => {
+        if (err) throw err;
+      });
+      console.log('newFiles ' + newFiles);
+      newFiles = newFiles.length;
+      expect(newFiles.to.eql(files));
       done();
     });
   });
